de7822ceccc41c9b4309c10b7e96e5ec27bab2b1
Tristan Batchler <info@tbat.me>
2024-12-06 18:21:39 +1000

    Remove debug

--- _plugins/imagefilter.rb
+++ _plugins/imagefilter.rb
@@ -2,18 +2,18 @@ require 'fastimage'
 
 module ImageSizeFilter
   def image_size(source, dimension = nil)
-    puts "supplied source: #{source}"
+    # puts "supplied source: #{source}"
 
     # Check if the source is a URL
     is_url = source.start_with?('http://', 'https://')
 
-    puts "is_url: #{is_url}"
+    # puts "is_url: #{is_url}"
 
     # If it's not a URL, resolve it to a local file path
     unless is_url
       # For local development, use the `_site` folder as the base
       source = File.join(Dir.pwd, '_site', source.sub(/^\//, ''))
-      puts "was not a url, so updated source: #{source}"
+      # puts "was not a url, so updated source: #{source}"
     end
 
     # Get image dimensions using FastImage
