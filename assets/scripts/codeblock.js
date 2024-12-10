5b8c34592c13560cbecbe268d9a45787be756548
Tristan Batchler <info@tbat.me>
2024-12-10 16:45:59 +1000

    Remove jquery and rewrite js to vanilla

--- assets/scripts/codeblock.js
+++ assets/scripts/codeblock.js
@@ -1,6 +1,6 @@
 // Add $ to shell code blocks
-$('div.language-shell code').each(function() {
-  let contents = $(this).html();
+document.querySelectorAll('div.language-shell code').forEach(function (codeBlock) {
+  let contents = codeBlock.innerHTML;
   let lines = contents.split('\n');
 
   // Remove the last line if it's empty
@@ -8,41 +8,47 @@ $('div.language-shell code').each(function() {
     lines.pop();
   }
   
-  let updatedLines = lines.map(function(line) {
+  // Add the $ to each line
+  let updatedLines = lines.map(function (line) {
     return '<span class="sh-p">$ </span>' + line;
   });
 
-  $(this).html(updatedLines.join('\n'));
-
+  // Set the updated HTML back to the code block
+  codeBlock.innerHTML = updatedLines.join('\n');
 });
 
 // Add copy button to all code blocks
-var codeBlocks = document.querySelectorAll('pre.highlight');
-codeBlocks.forEach(function (codeBlock) {
+document.querySelectorAll('pre.highlight').forEach(function (codeBlock) {
+  // Create the copy button
   var button = document.createElement('button');
   button.className = 'copy-button';
   button.type = 'button';
   button.ariaLabel = 'Copy code to clipboard';
   button.innerText = 'Copy';
 
+  // Prepend the button to the code block
   codeBlock.prepend(button);
 
+  // Add event listener for copy functionality
   button.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
 
     // Check if the ancestor has the .language-shell class
-    if (codeBlock.parentElement && codeBlock.parentElement.parentElement &&
+    if (codeBlock.parentElement &&
+        codeBlock.parentElement.parentElement &&
         codeBlock.parentElement.parentElement.classList.contains('language-shell')) {
+      // Remove the $ from the start of lines before copying
       code = code.replace(/^\$ /gm, '');
     }
 
-    window.navigator.clipboard.writeText(code);
-
-    button.innerText = 'Copied';
-    var fourSeconds = 4000;
-
-    setTimeout(function () {
-      button.innerText = 'Copy';
-    }, fourSeconds);
+    // Copy to clipboard
+    window.navigator.clipboard.writeText(code).then(function() {
+      button.innerText = 'Copied';
+      setTimeout(function () {
+        button.innerText = 'Copy';
+      }, 4000);
+    }).catch(function() {
+      button.innerText = 'Failed';
+    });
   });
 });
